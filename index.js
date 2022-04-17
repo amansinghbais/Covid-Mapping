30f7f0e15fd43bc3c16e6c60911bc88fa50e849f
Aman Singh Bais <69574321+amansinghbais@users.noreply.github.com>
2022-04-18 01:52:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.use(express.static('public'))
 app.set('view engine' , 'hbs')
